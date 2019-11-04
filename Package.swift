97aa6e9b6154656190e0949bcc45f44fb65aec88
Erik Terwan <erik@fullst-ck.dev>
2019-11-04 12:01:27 +0100

    Added SPM support (#7)
    * Update Package.swift
    
    Updated Package.swift to allow for use in SPM w. Xcode 11

--- Package.swift
+++ Package.swift
@@ -1,5 +1,28 @@
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
-    name: "Cachyr"
+    name: "Cachyr",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
+    ],
+    products: [
+        .library(
+            name: "Cachyr",
+            targets: ["Cachyr"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Cachyr",
+            dependencies: [],
+            path: "Sources"),
+        .testTarget(
+            name: "CachyrTests",
+            dependencies: ["Cachyr"]),
+    ],
+    swiftLanguageVersions: [.v5]
 )
